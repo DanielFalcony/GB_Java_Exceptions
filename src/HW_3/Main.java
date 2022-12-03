0d01c0d9852beef54a9b56d1cb2c30734b5b9d02
Daniil <danielfalcony@gmail.com>
2022-12-03 18:02:02 +0300

    HW_3 Done

---
+++ src/HW_3/Main.java
@@ -0,0 +1,13 @@
+package HW_3;
+
+import static HW_3.Services.writeInUserFile;
+
+public class Main {
+    public static void main(String[] args) {
+        // 12.12.12 Иванов 9256487412 Иванович Иван м
+        // 12.12.12 Сидоров 89254787пп Иванович Петр м
+        // Анна ж 12.12.12 Петровна Сидорова 7632648712
+        // ж 0090878769764 Петрова 22.09.45 Сергеевна Лидия
+        writeInUserFile();
+    }
+}
